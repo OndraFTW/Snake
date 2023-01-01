39a9fea0bef3a3be937fadb504d918710a9040fb
Ondrej Slampa <oslampa@gmail.com>
2023-01-01 17:45:35 +0100

    game control moved to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,129 @@
 import React from "react";
-import Game from "./Game";
+import Game, { GameState, GameStatus, Direction } from "./Game";
+import styles from "./App.module.scss";
+
+enum AppStatus {
+  Start,
+  Lost,
+  Won,
+  Replay,
+}
 
 interface AppProps {}
 
 interface AppState {
   side: number;
   tick: number;
+  status: AppStatus;
+  score: number;
+  direction: Direction;
+  gameStatus: GameStatus;
 }
 
 export default class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
+    this.gameFinished = this.gameFinished.bind(this);
+    window.addEventListener("keydown", (e: KeyboardEvent) => {
+      this.buttonPressed(e);
+    });
     this.state = this.getBaseState();
+  }
+
+  buttonPressed(e: KeyboardEvent) {
+    const newState = { ...this.state };
+    switch (e.key) {
+      case "ArrowLeft": {
+        newState.direction = Direction.Left;
+        break;
+      }
+      case "ArrowRight": {
+        newState.direction = Direction.Right;
+        break;
+      }
+      case "ArrowUp": {
+        newState.direction = Direction.Up;
+        break;
+      }
+      case "ArrowDown": {
+        newState.direction = Direction.Down;
+        break;
+      }
+      case "Enter": {
+        if (newState.gameStatus === GameStatus.Stop) {
+          newState.direction = Direction.None;
+          newState.gameStatus = GameStatus.Start;
+        }
+        break;
+      }
+      case "Shift": {
+        if (newState.gameStatus === GameStatus.Stop) {
+          newState.direction = Direction.None;
+          newState.gameStatus = GameStatus.Replay;
+        }
+        break;
+      }
+    }
+    this.setState(newState);
   }
 
   getBaseState(): AppState {
     return {
       side: 16,
       tick: 400,
+      status: AppStatus.Start,
+      score: 0,
+      direction: Direction.None,
+      gameStatus: GameStatus.Start,
     };
   }
 
+  gameFinished(gameState: GameState) {
+    console.log(gameState.fruitHistory);
+    console.log(gameState.directionHistory);
+    const newState = { ...this.state };
+    newState.score = gameState.score;
+    newState.status = gameState.won ? AppStatus.Won : AppStatus.Lost;
+    newState.gameStatus = GameStatus.Stop;
+    this.setState(newState);
+  }
+
   render() {
-    return <Game side={this.state.side} tick={this.state.tick} />;
+    var statusBar: React.ReactNode;
+    if (this.state.status === AppStatus.Start) {
+      statusBar = <div className={styles.statusBar}>Use arrow keys.</div>;
+    } else if (this.state.status === AppStatus.Won) {
+      statusBar = (
+        <div className={styles.statusBar}>
+          You won!
+          <br />
+          Press Enter to restart or Shift to see replay.
+        </div>
+      );
+    } else if (this.state.status === AppStatus.Lost) {
+      statusBar = (
+        <div className={styles.statusBar}>
+          You lost!
+          <br />
+          Press Enter to restart or Shift to see replay.
+        </div>
+      );
+    } else {
+      statusBar = (
+        <div className={styles.statusBar}>Score: {this.state.score}</div>
+      );
+    }
+    return (
+      <div className={styles.app}>
+        <Game
+          side={this.state.side}
+          tick={this.state.tick}
+          finished={this.gameFinished}
+          direction={this.state.direction}
+          status={this.state.gameStatus}
+        />
+        {statusBar}
+      </div>
+    );
   }
 }
--- src/Game.tsx
+++ src/Game.tsx
@@ -2,42 +2,47 @@ import React from "react";
 import Board from "./Board";
 import styles from "./App.module.scss";
 
-export enum GameStatus {
+export enum Direction {
+  None,
   Left,
   Right,
   Up,
   Down,
+}
+
+export enum GameStatus {
   Start,
-  Won,
-  Lost,
+  Stop,
   Replay,
 }
 
-interface GameState {
+export interface GameState {
   round: number;
   score: number;
-  status: GameStatus;
-  buttonPushed: boolean;
+  won: boolean;
+  replayStarted: boolean;
   snake: [number, number][];
   fruit: [number, number];
   fruitHistory: [number, number][];
-  statusHistory: GameStatus[];
+  directionHistory: Direction[];
 }
 
 interface GameProps {
   side: number;
   tick: number;
+  status: GameStatus;
+  direction: Direction;
+  finished: (gameState: GameState) => void;
 }
 
 export default class Game extends React.Component<GameProps, GameState> {
   interval?: NodeJS.Timer;
+  firstFruit: [number, number];
 
   constructor(props: GameProps) {
     super(props);
-    window.addEventListener("keydown", (e: KeyboardEvent) => {
-      this.buttonPressed(e);
-    });
     this.state = this.getBaseState();
+    this.firstFruit = this.state.fruit;
   }
 
   componentDidMount() {
@@ -52,68 +57,82 @@ export default class Game extends React.Component<GameProps, GameState> {
     this.gameStep();
   }
 
-  buttonPressed(e: KeyboardEvent) {
-    const statusAfterButton = this.buttonToState(e, this.state.status);
-    if (statusAfterButton === GameStatus.Start) {
-      this.setState(this.getBaseState());
-    } else if (statusAfterButton === GameStatus.Replay) {
-      this.setState(this.getReplayState());
-    } else if (!this.state.buttonPushed) {
-      const newState = { ...this.state };
-      newState.status = statusAfterButton;
-      newState.buttonPushed = true;
-      this.setState(newState);
-    }
-  }
-
   gameStep() {
     if (
-      [GameStatus.Start, GameStatus.Won, GameStatus.Lost].includes(
-        this.state.status
-      )
+      this.props.status === GameStatus.Start &&
+      this.props.direction === Direction.None
     ) {
+      this.setState(this.getBaseState());
+      return;
+    }
+    if (this.props.status === GameStatus.Stop) {
       return;
     }
     const newState = { ...this.state };
-    newState.buttonPushed = false;
+    if (
+      this.props.status === GameStatus.Replay &&
+      this.props.direction === Direction.None &&
+      !newState.replayStarted
+    ) {
+      newState.round = 0;
+      newState.score = 0;
+      newState.won = false;
+      newState.replayStarted = true;
+      newState.snake = [[this.props.side - 1, this.props.side / 2]];
+      newState.fruit = newState.fruitHistory[0];
+    }
     newState.round++;
-    const newHead = this.getNewHead(
-      newState.status === GameStatus.Replay
-        ? this.state.statusHistory[newState.round]
-        : newState.status,
-      this.state.snake[0]
-    );
+    const newDirection =
+      this.props.status === GameStatus.Replay
+        ? newState.directionHistory[newState.round]
+        : this.props.direction;
+    const newHead = this.getNewHead(newDirection, newState.snake[0]);
     const ateFruit =
-      newHead[0] === this.state.fruit[0] && newHead[1] === this.state.fruit[1];
+      newHead[0] === newState.fruit[0] && newHead[1] === newState.fruit[1];
     newState.snake = [
       newHead,
-      ...this.state.snake.slice(
+      ...newState.snake.slice(
         0,
-        ateFruit ? this.state.snake.length : this.state.snake.length - 1
+        ateFruit ? newState.snake.length : newState.snake.length - 1
       ),
     ];
-    if (this.state.status !== GameStatus.Replay) {
-      newState.statusHistory = [...newState.statusHistory, newState.status];
-    }
-    if (
-      newState.status === GameStatus.Replay &&
-      newState.statusHistory &&
-      newState.statusHistory.length < newState.round
-    ) {
-      newState.status = this.state.statusHistory[newState.round];
-    } else {
-      newState.status = this.getNewGameStatus(newState.snake, newState.status);
+    if (this.props.status !== GameStatus.Replay) {
+      newState.directionHistory = [
+        ...newState.directionHistory,
+        this.props.direction,
+      ];
     }
     if (ateFruit) {
       newState.score++;
-      if (this.state.status !== GameStatus.Replay) {
+      if (this.props.status !== GameStatus.Replay) {
         newState.fruit = this.getNewFruit(newState.snake);
         newState.fruitHistory = [...newState.fruitHistory, newState.fruit];
       } else {
-        newState.fruit = this.state.fruitHistory[newState.score];
+        newState.fruit = newState.fruitHistory[newState.score];
       }
     }
+    var gameEnded = false;
+    if (newState.snake.length === this.props.side * this.props.side) {
+      newState.won = true;
+      gameEnded = true;
+      newState.replayStarted = false;
+    } else if (
+      newHead[0] === -1 ||
+      newHead[0] === this.props.side ||
+      newHead[1] === -1 ||
+      newHead[1] === this.props.side ||
+      newState.snake
+        .slice(1, newState.snake.length)
+        .find((p) => p[0] === newHead[0] && p[1] === newHead[1])
+    ) {
+      newState.won = false;
+      gameEnded = true;
+      newState.replayStarted = false;
+    }
     this.setState(newState);
+    if (gameEnded) {
+      this.props.finished(newState);
+    }
   }
 
   getNewFruit(snake: [number, number][]) {
@@ -127,59 +146,18 @@ export default class Game extends React.Component<GameProps, GameState> {
     return newFruit;
   }
 
-  getNewHead(key: GameStatus, head: [number, number]): [number, number] {
-    switch (key) {
-      case GameStatus.Left:
+  getNewHead(direction: Direction, head: [number, number]): [number, number] {
+    switch (direction) {
+      case Direction.Left:
         return [head[0], head[1] - 1];
-      case GameStatus.Right:
+      case Direction.Right:
         return [head[0], head[1] + 1];
-      case GameStatus.Up:
+      case Direction.Up:
         return [head[0] - 1, head[1]];
-      case GameStatus.Down:
+      case Direction.Down:
         return [head[0] + 1, head[1]];
       default:
         return head;
-    }
-  }
-
-  getNewGameStatus(snake: [number, number][], status: GameStatus) {
-    if (snake.length === this.props.side * this.props.side) {
-      return GameStatus.Won;
-    }
-    const head = snake[0];
-    if (
-      head[0] === -1 ||
-      head[0] === this.props.side ||
-      head[1] === -1 ||
-      head[1] === this.props.side ||
-      snake
-        .slice(1, snake.length)
-        .find((p) => p[0] === head[0] && p[1] === head[1])
-    ) {
-      return GameStatus.Lost;
-    }
-    return status;
-  }
-
-  buttonToState(e: KeyboardEvent, status: GameStatus): GameStatus {
-    if ([GameStatus.Won, GameStatus.Lost].includes(this.state.status)) {
-      if (e.key === "Enter") {
-        return GameStatus.Start;
-      } else if (e.key === "Shift") {
-        return GameStatus.Replay;
-      } else {
-        return status;
-      }
-    } else if (e.key === "ArrowLeft" && status !== GameStatus.Right) {
-      return GameStatus.Left;
-    } else if (e.key === "ArrowRight" && status !== GameStatus.Left) {
-      return GameStatus.Right;
-    } else if (e.key === "ArrowUp" && status !== GameStatus.Down) {
-      return GameStatus.Up;
-    } else if (e.key === "ArrowDown" && status !== GameStatus.Up) {
-      return GameStatus.Down;
-    } else {
-      return status;
     }
   }
 
@@ -187,69 +165,27 @@ export default class Game extends React.Component<GameProps, GameState> {
     const snake: [number, number][] = [
       [this.props.side - 1, this.props.side / 2],
     ];
-    const fruit = this.getNewFruit(snake);
+    const fruit = this.firstFruit ? this.firstFruit : this.getNewFruit(snake);
     return {
       round: 0,
       score: 0,
       fruit,
-      buttonPushed: false,
-      status: GameStatus.Start,
+      won: false,
+      replayStarted: false,
       fruitHistory: [fruit],
-      statusHistory: [GameStatus.Start],
+      directionHistory: [Direction.None],
       snake,
     };
-  }
-
-  getReplayState(): GameState {
-    const snake: [number, number][] = [
-      [this.props.side - 1, this.props.side / 2],
-    ];
-    const newState = {
-      round: 0,
-      score: 0,
-      buttonPushed: false,
-      status: GameStatus.Replay,
-      fruit: this.state.fruitHistory[0],
-      snake,
-      fruitHistory: this.state.fruitHistory,
-      statusHistory: this.state.statusHistory,
-    };
-    return newState;
   }
 
   render(): React.ReactNode {
-    var statusBar: React.ReactNode;
-    if (this.state.status === GameStatus.Start) {
-      statusBar = <div className={styles.statusBar}>Use arrow keys.</div>;
-    } else if (this.state.status === GameStatus.Won) {
-      statusBar = (
-        <div className={styles.statusBar}>
-          You won!
-          <br />
-          Press Enter to restart or Shift to see replay.
-        </div>
-      );
-    } else if (this.state.status === GameStatus.Lost) {
-      statusBar = (
-        <div className={styles.statusBar}>
-          You lost!
-          <br />
-          Press Enter to restart or Shift to see replay.
-        </div>
-      );
-    } else {
-      statusBar = (
-        <div className={styles.statusBar}>Score: {this.state.score}</div>
-      );
-    }
     return (
-      <div className={styles.app}>
+      <div className={styles.game}>
         <Board
           side={this.props.side}
           snake={this.state.snake}
           fruit={this.state.fruit}
         />
-        {statusBar}
       </div>
     );
   }
